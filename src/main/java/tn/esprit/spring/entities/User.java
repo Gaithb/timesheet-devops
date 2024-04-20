f737bb6089e872de3df6d3df683b068f09d2b5f3
mgbasly <mohamedgaith.basly@esprit.tn>
2024-04-20 11:16:01 +0100

    Changement user.java

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
 
 
 	private static final long serialVersionUID = 1L;
-
+//Premier changement :
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;   
