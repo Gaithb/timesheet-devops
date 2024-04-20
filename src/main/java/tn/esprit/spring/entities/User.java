bf4a66d75379a43b5712605e43d372cf4ea351cf
mgbasly <mohamedgaith.basly@esprit.tn>
2024-04-20 11:43:18 +0100

    Coorection2

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
 
 
 	private static final long serialVersionUID = 1L;
-//Premier changement :
+//Premier changement : -- 2
 //  Nouveau commentaire --
 //3rd comment
 	@Id
